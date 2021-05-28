3028759a093a18ba8abb5a21c30081c4e4dbcfe7
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-05-28 09:58:21 +0200

    Add product.reducer and import it in product.module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,6 +7,7 @@ import { ProductShellComponent } from './product-shell/product-shell.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { StoreModule } from '@ngrx/store';
+import { productReducer } from './state/product.reducer';
 
 const productRoutes: Routes = [
   { path: '', component: ProductShellComponent }
@@ -16,7 +17,7 @@ const productRoutes: Routes = [
   imports: [
     SharedModule,
     RouterModule.forChild(productRoutes),
-    StoreModule.forFeature('products', {})
+    StoreModule.forFeature('products', productReducer)
   ],
   declarations: [
     ProductShellComponent,
