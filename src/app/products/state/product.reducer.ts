e83c877be83a20a522e8cba4bc728563fbb23d0e
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-05-31 14:28:56 +0200

    Add selectors to product state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
 import { Product } from "../product";
 
 import * as AppState from '../../state/app.state'
@@ -17,6 +17,23 @@ const initialState: ProductState = {
   currentProduct: null,
   products: []
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
 
 export const productReducer = createReducer<ProductState>(
   initialState,
