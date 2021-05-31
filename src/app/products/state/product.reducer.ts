7c5c455ec8931f4b0eaa9565537438ac4a5ee025
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-05-31 13:31:20 +0200

    Add initial Product state to get typed help

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -12,9 +12,14 @@ export interface ProductState {
   products: Product[];
 }
 
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+}
 
 export const productReducer = createReducer<ProductState>(
-  { showProductCode: true } as ProductState,
+  initialState,
   on(createAction('[Product] Toggle Product Code'), (state):  ProductState => {
     console.log('original state: ', JSON.stringify(state));
     return {
