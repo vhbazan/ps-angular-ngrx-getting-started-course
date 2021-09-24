ce70b27e72ed379c4fe8593d748128aa117a540b
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-09-24 13:47:05 +0200

    Add product shell component

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Product } from '../product';
+import { getCurrentProduct, getShowProductCode, State, getProducts, getError } from '../state';
+import * as ProductActions from "../state/product.actions";
+
 
 @Component({
   templateUrl: './product-shell.component.html'
 })
 export class ProductShellComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  errorMessage$: Observable<string>;
+  displayCode$: Observable<boolean>;
 
-  ngOnInit() {
+  constructor(
+    private store: Store<State>
+      ) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+    this.displayCode$ = this.store.select(getShowProductCode);
+    this.errorMessage$ = this.store.select(getError)
+  }
+
+  checkChanged(): void {
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.store.dispatch(ProductActions.initCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
   }
 }
